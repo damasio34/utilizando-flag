deb5e6ba9e61898c236ec015e111026f633e378c
Darlan Damasio <damasio34@gmail.com>
2016-04-01 08:29:24 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace utilizando_flag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<Status> estahAprovado = (status) => Console.WriteLine("O status {0} é aprovado? {1}", status, status.HasFlag(Status.Aprovado));
+
+            estahAprovado(Status.AguardandoAprovacao);
+            estahAprovado(Status.Rejeitado);
+            estahAprovado(Status.Cancelado);
+            estahAprovado(Status.Aprovado);
+            estahAprovado(Status.ParcialmenteAtendido);
+            estahAprovado(Status.TotalmenteAtendido);
+            estahAprovado(Status.EmAtendimento);
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Status.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace utilizando_flag
+{
+    [Flags]
+    public enum Status
+    {
+        AguardandoAprovacao = 0,
+        Rejeitado = 1,
+        Cancelado = 2,
+        Aprovado = 64,
+        ParcialmenteAtendido = Aprovado + 9,
+        TotalmenteAtendido = Aprovado + 2,
+        EmAtendimento = Aprovado + 3
+    }
+}
